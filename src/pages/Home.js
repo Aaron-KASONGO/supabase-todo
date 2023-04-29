fca7bcd7a63771cf3afc48b75bfd90767ad81e94
Aaron KASONGO <19kk181@esisalama.org>
2023-04-30 01:09:08 +0200

    adding fetching data simply

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,42 @@
+import supabase from "../config/SupabaseClient"
+import { useState, useEffect } from 'react'
+
+
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  const fetchSmoothies = async () => {
+    const {data, error} = await supabase
+      .from('smoothies')
+      .select()
+
+      if (error) {
+        setFetchError('Could not fetch the smoothies');
+        setSmoothies(null);
+      }
+
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+  }
+
+  useEffect(() => {
+    
+    fetchSmoothies();
+  }, []);
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && (<p>{fetchError}</p>)}
+      {smoothies && (
+        <div className="smoothies">
+          {smoothies.map(smoothie => (
+            <p key={smoothie.id}>{smoothie.title}</p>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
