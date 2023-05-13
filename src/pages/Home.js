b56c6399519518697183e4cfc33a5de554a21d04
Aaron KASONGO <19kk181@esisalama.org>
2023-05-13 17:38:29 +0200

    adding google and github connexion

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,8 @@ const Home = () => {
   const [fetchError, setFetchError] = useState(null);
   const [smoothies, setSmoothies] = useState(null);
 
+  const [user, setUser] = useState(null);
+
   const fetchSmoothies = async () => {
     const {data, error} = await supabase
       .from('smoothies')
@@ -23,20 +25,57 @@ const Home = () => {
   }
 
   useEffect(() => {
-    
+    const data = supabase.auth.getUser();
+
+    supabase.auth.onAuthStateChange((event, session) => {
+      switch (event) {
+        case 'SIGNED_IN':
+          data.then((response) => setUser(response.data.user))
+          break;
+        
+        case 'SIGNED_OUT':
+          setUser(null)
+        default:
+          break;
+      }
+    })
+
     fetchSmoothies();
   }, []);
 
+  const login = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google'
+    })
+  }
+
+  const logout = async () => {
+    await supabase.auth.signOut()
+  }
+
   return (
     <div className="page home">
-      {fetchError && (<p>{fetchError}</p>)}
-      {smoothies && (
-        <div className="smoothies">
-          {smoothies.map(smoothie => (
-            <p key={smoothie.id}>{smoothie.title}</p>
-          ))}
-        </div>
-      )}
+      {
+        user ?
+        (
+
+          // fetchError && (<p>{fetchError}</p>)
+          smoothies && (
+            <div className="smoothies">
+              {smoothies.map(smoothie => (
+                <p key={smoothie.id}>{smoothie.title}</p>
+              ))}
+              <button onClick={logout}>logout</button>
+            </div>
+          )
+        ):
+        (
+          <div>
+            <button onClick={login}>Login with github</button>
+            
+          </div>
+        )
+      }
     </div>
   )
 }
